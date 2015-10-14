d23ec15fb84f7386329f2c599fe6295ef1e54867
Warren <warren@methodsdigital.co.uk>
2015-10-14 12:12:21 +0100

    DIGEQ-59 render multiple choice single answer. Refactoring the json_forms.py to deal with the new schema that also includes multiple choice questions.

--- jsonforms.py
+++ jsonforms.py
@@ -15,19 +15,57 @@ def slugify(text, delim=u'_'):
         result.extend(unidecode(word).split())
     return unicode(delim.join(result))
 
+
 class Converter(object):
     """docstring for Converter"""
 
-    def convert(self, question):
+    field = {}
+
+    def __init__(self, question):
+        print question
+        questiontype = question['questionType']
+
+        if questiontype == "InputText":
+            self.convert_textfield(question)
+        elif questiontype == "MultipleChoice":
+            self.convert_radiofield(question)
+        return
+
+    def get_field(self):
+        return self.field
+
+    def convert_textfield(self, question):
         """Given a question dict structure, return a TextField"""
         kwargs = {
-            '_name': slugify(question['title']),
-            'label': question['title'],
-            'description': question['description'],
+            '_name': slugify(question['questionText']),
+            'label': question['questionText'],
+            'description': question['questionHelp'],
             'validators': [],
             'filters': [],
         }
-        return  f.StringField( **kwargs)
+        self.field = f.StringField(**kwargs)
+
+    def convert_radiofield(self, question):
+        """Given a question dict structure, return a TextField"""
+        kwargs = {
+            '_name': slugify(question['questionText']),
+            'label': question['questionText'],
+            'description': question['questionHelp'],
+            'choices': self.get_choices(question['parts']),
+            'validators': [],
+            'filters': [],
+        }
+        self.field = f.RadioField(**kwargs)
+
+    def get_choices(self, parts):
+        choices = []
+        for part in parts:
+            value = part['value']
+            name = part['name']
+            choice = (name, value)
+            choices.append(choice)
+        return choices
+
 
 
 def json_fields(form_schema):
@@ -35,12 +73,13 @@ def json_fields(form_schema):
     Given a form schema in json, return a dictionary of fields,
     having called the converter on each field.
     """
-    converter = Converter()
+
     field_dict = {}
     for question in form_schema['questions']:
-        field = converter.convert(question)
+        converter = Converter(question)
+        field = converter.get_field()
         if field is not None:
-            field_dict[slugify(question['title'])] = field
+            field_dict[slugify(question['questionText'])] = field
 
     return field_dict
 
