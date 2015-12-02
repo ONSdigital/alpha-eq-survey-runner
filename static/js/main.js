e626a197dd5e70c89f23843f9754884d5983ac1c
Alex Morris <alexmorris@me.com>
2015-12-02 14:28:44 +0000

    combined inputs into single text field

--- static/js/main.js
+++ static/js/main.js
@@ -10,7 +10,9 @@ function monthChanged() {
   var month = $('.month').val() - 1,
     year = +$('.year').val();
   // Check for leap year if Feb
-  if (month == 1 && new Date(year, month, 29).getMonth() == 1) days[1]++;
+  if (month === 1 && new Date(year, month, 29).getMonth() === 1) {
+    days[1]++;
+  }
   $(".day").empty();
   for (var i = $('.day option').length + 1; i <= days[month]; i++) {
     $('<option>').attr('value', i).text(i).appendTo('.day');
@@ -19,7 +21,7 @@ function monthChanged() {
 
 function checkLeap() {
   var month = $('.month').val() - 1;
-  if (month == 1) {
+  if (month === 1) {
     monthChanged();
   }
 }
@@ -30,7 +32,16 @@ $(function() {
   $('.year').change(checkLeap);
   var currentYear = (new Date).getFullYear();
   $('.year').val(currentYear);
-  $(".year").on("click", function() {
-    $(this).val("")
+  $('.year').on("click", function() {
+    $(this).val("");
   });
+
+  $('.date-input').change(function() {
+    $(this).next($('.combined-date')).val($('.month').val() + "/" +
+      $(
+        '.day option:selected')
+      .text() +
+      "/" + $('.year').val());
+  });
+
 });
